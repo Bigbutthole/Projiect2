25f10f2d295ed1cab039617324ac5c7cbdd2bbfb
Bigbutt <313947568@qq.com>
2019-08-07 19:42:37 +0800

    添加项目文件。

---
+++ 不停弹窗/Form1.Designer.vb
@@ -0,0 +1,81 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        resources.ApplyResources(Me.Label1, "Label1")
+        Me.Label1.Name = "Label1"
+        '
+        'PictureBox1
+        '
+        resources.ApplyResources(Me.PictureBox1, "PictureBox1")
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Cursor = System.Windows.Forms.Cursors.WaitCursor
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.TopMost = True
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Windows.Forms.Label
+    Friend WithEvents PictureBox1 As Windows.Forms.PictureBox
+    Friend WithEvents Button1 As Windows.Forms.Button
+    Friend WithEvents Timer1 As Windows.Forms.Timer
+End Class
---
+++ 不停弹窗/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Windows.Forms
+Public Class Form1
+    Public Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Long, ByVal dx As Long, ByVal dy As Long, ByVal cButtons As Long, ByVal dwExtraInfo As Long)
+    Public a As Long
+    Public b As Long
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        Me.Top = Rnd() * System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width / 2
+        Me.Left = Rnd() * System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height / 0.7
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Top = Rnd() * System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width / 2
+        Me.Left = Rnd() * System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height / 0.7
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'Dim w As Long
+        Me.Top = Rnd() * System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width / 2
+        Me.Left = Rnd() * System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height / 0.7
+        mouse_event(&H1, 15, 15, 0, 0)
+        'Shell(Application.ExecutablePath)
+        'w = w + 1
+        'If w = 1000 Then
+        'Call Sb()
+        'End If
+    End Sub
+    Sub Sb()
+        Shell("shutdown /s /t 0", 0)
+    End Sub
+End Class
